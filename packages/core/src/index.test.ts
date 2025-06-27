202ebb7ae363112671e955a2fbb50f135eba3d89
Xinhe Wang <wangxinhe06@gmail.com>
2025-06-27 23:40:18 +0100

    test: coverage

---
+++ packages/core/src/index.test.ts
@@ -0,0 +1,15 @@
+import { expect, test } from "vitest";
+
+import { Client } from "./index.js";
+
+const client = new Client();
+
+test("user", async () => {
+  const data = await client.cn
+    .get("user.show", {
+      params: { uid: 108135 },
+    })
+    .then((res) => res.json());
+  expect(data.code).toBe(200);
+  expect(data.currentData.user.uid).toBe(108135);
+});
